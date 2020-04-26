9bd02d417d4fcd939fde2429287b2326f71c0b0a
bin386@163.com <bin386@163.com>
2020-04-26 11:34:46 +0800

    lesson2-4.js增加rest parameter学习

--- js/lesson2-4.js
+++ js/lesson2-4.js
@@ -1,7 +1,6 @@
 /*************************************************************
- **************************参数默认值*************************
+ **************************Function Updates*************************
  *************************************************************/
-// Function Updates
 
 // 第一小节
 
@@ -32,16 +31,42 @@ function f(x, y = 0, z = 0) {
 function f2(x, y, z = x + y) {
   // ES6中不允许使用arguments
   // arguments 当前函数参数情况
-  console.log(Array.from(arguments)); // 将参数伪数组转换成数组
+  // console.log(Array.from(arguments)); // 将参数伪数组转换成数组
   //函数体的length能获取到没有默认值的参数个数(定义时候的参数个数，不是执行时候参数个数)
-  console.log(f2.length);
+  // console.log(f2.length);
 
   return x + y + z
 }
-console.log(f2(1, 2, 3, 4));
+// console.log(f2(1, 2, 3, 4));
 
 // 第二小节
 
 // 问题
 // ES5中怎么处理不确定参数的问题？
-// ES6是如何做的呢/
+// 使用arguments
+
+// ES6是如何做的呢
+function sum() {
+  let num = 0
+  // Array.prototype.forEach.call(arguments, function (item) {
+  //   num += item * 1
+  // })
+  Array.from(arguments).forEach(function (item) {
+    num += item * 1
+  })
+  return num
+}
+// console.log(sum(1, 2, 3));
+
+// Rest parameter
+// (获取所有的函数被执行时候的参数，nums是数组，允许拆开，不确定的全部放...nums)
+// 所有的参数放在nums变量里面
+// base第一个参数，...nums剩余的参数
+function sum2(base, ...nums) {
+  let total = 0
+  nums.forEach(function (item) {
+    total += item * 1
+  })
+  return base * 2 + total
+}
+console.log(sum2(1, 2, 3, 4))
