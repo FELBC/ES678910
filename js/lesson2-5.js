5000b0eb3e0166940a7dcb12224989bb625b4280
bin386@163.com <bin386@163.com>
2020-04-26 15:36:07 +0800

    lesson2-5.js增加Set学习

--- js/lesson2-5.js
+++ js/lesson2-5.js
@@ -16,7 +16,7 @@ let obj = {
   }
 }
 obj[z] = 5 // key为变量
-obj.hello()
+// obj.hello()
 
 let obj2 = {
   x,
@@ -32,5 +32,29 @@ let obj2 = {
     console.log('hello2')
   }
 }
-obj2.hello2() //无输出
-obj2.hello2().next() //hello2
+// obj2.hello2() //无输出
+// obj2.hello2().next() //hello2
+
+// Set 
+// Set本质上还是obj对象
+// Set存储的成员不允许是重复的
+// Set接收的参数是一个可遍历的对象
+let s = new Set()
+// let s = new Set([1, 2, 3, 4])
+s.add('hello').add('goodbye').add('hello') // 存入重复数据会帮你自动过滤
+// console.log(s); //Set(2) {"hello", "goodbye"}
+// s.delete('hello')
+// console.log(s)
+// s.clear() // 全部清空
+// console.log(s)
+// console.log(s.has('hello')); // 判断是否存在某条数据
+// console.log(s.size); // 查询有多少条数据
+// console.log(s.keys());
+// console.log(s.values());
+// console.log(s.entries());
+// s.forEach((item) => {
+//   console.log(item)
+// })
+for (let item of s) {
+  console.log(item)
+}
