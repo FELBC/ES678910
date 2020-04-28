b3312365c57a9f7d2c7038f8c43ea12f0351d0e8
bin386@163.com <bin386@163.com>
2020-04-28 17:54:18 +0800

    新增lesson4-3.js,lesson4-4.js,增加String补白，obj描述符学习

--- js/lesson4-2.js
+++ js/lesson4-2.js
@@ -2,5 +2,21 @@
  *****************************ES8****************************
  *************************************************************/
 
- 
 // ES8中增加了对Object快速遍历的方法，了解下？
+
+let grade = {
+  'lilei':96,
+  'hanmeimei':99
+}
+
+for(let [k,v] of Object.entries(grade)){
+  console.log(k,v)
+}
+
+for(let k of Object.keys(grade)){
+  console.log(k)
+}
+
+for(let v of Object.values(grade)){
+  console.log(v)
+}
